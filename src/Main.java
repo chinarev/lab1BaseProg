12c8de7ed52738d91e391a022d0e69272b114413
Mike <mihaelchinarev@gmail.com>
2020-02-09 23:07:38 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Лабораторная работа №1.
+ * @author Mike Chinarev
+ * @version 1.0
+ */
+public class Main {
+    /** точка входа в программу */
+    public static void main(String[] args) {
+        /** Печать в консоль */
+        System.out.println("hi");
+    }
+}
